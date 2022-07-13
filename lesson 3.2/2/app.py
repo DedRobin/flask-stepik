e9c0ca878d3f9eac7ff8f22de0a31e560e6fff0b
Pavel Rabekin <pavelrabekin@gmail.com>
2022-07-13 12:00:47 +0300

    lesson 3.2/2

--- lesson 3.2/2/app.py
+++ lesson 3.2/2/app.py
@@ -8,9 +8,10 @@ def index():
     return "Flask-steppik"
 
 
-@app.route('/<int:a> <int:b> <int:c>/')
-def average(a, b, c):
-    return render_template('index.html', a=a, b=b, c=c)
+@app.route('/<a>/<sign>/<b>/')
+def arithmetic_operation(a, b, sign):
+    print(a, sign, b)
+    return render_template('index.html', a=a, b=b, sign=sign)
 
 
 if __name__ == '__main__':
