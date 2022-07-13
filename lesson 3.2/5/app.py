0c12ea769d31aa18a8fe205fabc668caf90831ee
Pavel Rabekin <pavelrabekin@gmail.com>
2022-07-13 13:51:57 +0300

    lesson 3.2/5

--- lesson 3.2/5/app.py
+++ lesson 3.2/5/app.py
@@ -8,10 +8,9 @@ def index():
     return "<h1>Flask-steppik</h1>"
 
 
-@app.route('/loop')
-def loop():
-    return render_template('index.html', numbers={'string': 0, 'f2': 1, '15': 6, 'g3': -8, '19': 19,
-                                                  'b2c5f': 11, '09': 90})
+@app.route('/test/')
+def test():
+    return render_template('index.html', email='popov@gmail.com', message='Flask - это прекрасно!', title='Обучение')
 
 
 if __name__ == '__main__':
